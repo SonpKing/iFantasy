fc8744aeb2b53f12c66c46bc58328a3e91236c7c
sonp <sonpking@gmail.com>
2018-04-25 23:22:30 +0800

    rm

--- app/controller/recommend.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Api, Resource, reqparse
-from app import db
-from app.model import Recruit, User, PlayerBase, BagPlayer, BagTrailCard, BagPiece, BagProp, Theme
-from .message import Message
-from random import choice, random
-import datetime
